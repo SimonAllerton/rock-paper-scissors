d647323ab59e411108b377036282f6bbde7c3cc7
Simon Allerton <129703971+SimonAllerton@users.noreply.github.com>
2023-09-14 15:46:55 -0700

    Will limit a players input to rock paper scissors
    This was done by adding to the if statement in the
    playRound function. I limited the user from inputting
    nothing and from inputting anything but rock paper
    or scissors.

--- index.js
+++ index.js
@@ -18,15 +18,19 @@ let computerSelection = getComputerChoice();
 
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
+    if (playerSelection == ''){
+        console.log('Wow there buddy! You need to enter a valid selection')
+    } else if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
+        console.log(`Seriously?! ${playerSelection} is not rock, paper, or scissors.`)
+    } else if (playerSelection === computerSelection) {
         console.log(`You tied! You both played ${playerSelection}.`)
-    }   else if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+    } else if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
                 (playerSelection == 'paper' && computerSelection == 'rock') ||
                 (playerSelection == 'scissors' && computerSelection == 'paper')) {
         console.log(`You Win! ${playerSelection} beats ${computerSelection}.`)
-        } else {
+    } else {
             console.log(`You Lost! ${computerSelection} beats ${playerSelection}.`)
-        }
+    }
 }
 
 // Prompts user to input a selection
