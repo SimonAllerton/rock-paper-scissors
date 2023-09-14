24017e3966e1b2c745e58894132c0faf9e5934e1
Simon Allerton <129703971+SimonAllerton@users.noreply.github.com>
2023-09-14 14:55:50 -0700

    Add a func that grabs a random computer selection
    This random selection is between rock, paper, and scissors.
    I used a switch statement that assigned 0, 1, & 2 to
    one of the above options.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getComputerChoice() {
+    // Get random number between 0 and 2
+    let randomChoice = Math.floor(Math.random() * 3);
+
+    // Return the corresponding choice
+    switch (randomChoice) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
+}
+
+// Runs getComputerChoice function
+let computerSelection = getComputerChoice();
+console.log(computerSelection);
+
