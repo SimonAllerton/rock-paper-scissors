bef5f7946b4d5141c84658e9e5ee789d7ba64bcf
Simon Allerton <129703971+SimonAllerton@users.noreply.github.com>
2023-09-15 08:59:56 -0700

    Adds a scoring system for the computer and player
    The game is not a best of game. It still gets played
    five times in a row no matter how many times you win
    or lose. One of the next commits should include a
    win/loss message at the end of the 5 games.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     // Get random number between 0 and 2
     let randomChoice = Math.floor(Math.random() * 3);
@@ -21,16 +24,21 @@ function playRound(playerSelection, computerSelection) {
     // if statement that evaluates your choice and the computer's choice
     if (playerSelection == ''){
         console.log('Wow there buddy! You need to enter a valid selection.')
+        return;
     } else if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
         console.log(`Seriously?! ${playerSelection} is not rock, paper, or scissors.`)
+        return;
     } else if (playerSelection === computerSelection) {
         console.log(`You tied! You both played ${playerSelection}.`)
+        return;
     } else if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
                 (playerSelection == 'paper' && computerSelection == 'rock') ||
                 (playerSelection == 'scissors' && computerSelection == 'paper')) {
         console.log(`You Win! ${playerSelection} beats ${computerSelection}.`)
+        return playerScore += 1;
     } else {
             console.log(`You Lost! ${computerSelection} beats ${playerSelection}.`)
+            return computerScore += 1;
     }
 }
 
@@ -44,10 +52,24 @@ function game(){
         let playerSelection = input.toLowerCase(); // Makes user input case sensitive
 
         playRound(playerSelection, computerSelection);
+
+        console.log(`Player Score = ${playerScore}`);
+        console.log(`Computer Score = ${computerScore}`);
+
         if (i === 5){
             break;
+        }
+       if (playerScore > computerScore) {
+            console.log('You Won!')
+            return 1;
+        } else {
+            console.log('You Lost!')
+            return 0;
         }
     }
 };
 
 game();
+
+
+
